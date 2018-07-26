c4b94af00ce1d749fa79a70cdcea834a5ab93cc8
allin <aa704538319@163.com>
2018-07-26 14:42:23 +0800

    add profile page and avatar

--- app/main/views.py
+++ app/main/views.py
@@ -53,6 +53,7 @@ def logout():
     logout_user()
     return redirect(url_for('main.index'))
 
+# 注册
 @main.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -68,3 +69,14 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('main.login'))
     return render_template('register.html', title='Register', form=form)
+
+# 查看user profile
+@main.route('/user/<username>')
+@login_required
+def user_profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
