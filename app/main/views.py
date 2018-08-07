6b60d985ea77a148abbc5a4ea581f66e4c801a18
allin <aa704538319@163.com>
2018-08-07 13:05:30 +0800

    添加查看私信功能和其思路图

--- app/main/views.py
+++ app/main/views.py
@@ -232,3 +232,21 @@ def send_message(recipient):
         flash('Your message has been sent')
         return redirect(url_for('main.user_profile', username=recipient))
     return render_template('send_message.html', title='Send Message', form=form, recipient=recipient)
+
+
+@main.route('/messages')
+@login_required
+def check_messages():
+    '''查看私信， 具有分页功能'''
+    current_user.last_message_read_time = datetime.utcnow()
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(
+        Message.timestamp.desc()).paginate(
+            page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.messages', page=messages.next_num) \
+        if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) \
+        if messages.has_prev else None
+    return render_template('messages.html', messages=messages.items,
+                           next_url=next_url, prev_url=prev_url)
